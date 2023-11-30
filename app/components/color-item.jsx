821d7484889c33ce8d4308621f0831b08b8bb668
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-11-30 12:01:09 +0100

    made chancheColor component hidden

--- app/components/color-item.jsx
+++ app/components/color-item.jsx
@@ -1,15 +1,33 @@
+'use client'
+
+import { useState } from "react";
 import { mockColors, mockColor, hexcolorRegex } from "@/tests/utils"
 import ChangeColor from "./changeColor"
 
 const ColorItem = () => {
+  const [isButtonClicked, setButtonClicked] = useState(false);
+  
+  const handleButtonClick = () => {
+    setButtonClicked(true);
+  };
+
+  const handleOnSubmit = () => {
+    setButtonClicked(false)
+  }
+
+
     return (
         <div>
             <li>
                 <h2>{mockColor.name}</h2>
-                <button>{mockColor.hex}</button>
+                {isButtonClicked ? (
+                   <ChangeColor onSubmit={handleOnSubmit} />
+                 ) : (
+                   
+                     <button onClick={handleButtonClick}>{mockColor.hex}</button>
+                   
+                 )}
 
-                < ChangeColor />
-             
             </li>
             
         </div>
