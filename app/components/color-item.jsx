2b9303caf6f784de08cd8a2781b4246af6cc3a4d
DavidDyberg <david.dyberg@edu.cmeducations.se>
2023-11-30 10:28:48 +0100

    work in progress

--- app/components/color-item.jsx
+++ app/components/color-item.jsx
@@ -1,10 +1,18 @@
-import { colorList } from "@/tests/utils"
+import { mockColors, mockColor, hexcolorRegex } from "@/tests/utils"
 
 const ColorItem = () => {
-    
     return (
         <div>
-            
+            <li>
+                <h2>{mockColor.name}</h2>
+                <button>{mockColor.hex}</button>
+
+                <form aria-label="change-color" >
+                    <input id="hex" name="hex-color" hidden />
+                    
+                </form>
+             
+            </li>
             
         </div>
     )
