8fe727ac38a3453465d3606e3132b641c0f11a43
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-11-30 10:26:27 +0100

    all tests passed

--- app/components/changeColor.jsx
+++ app/components/changeColor.jsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 
 const ChangeColor = ({ color, changeColor, disableEditing, inputRef }) => {
-  const [hexValue, setHexValue] = useState('');
+  const [hexValue, setHexValue] = useState("");
 
   const handleInputChange = (e) => {
     const newValue = e.target.value;
     setHexValue(newValue);
-    changeColor(newValue); // Call the changeColor function with the new hex value
+    changeColor(newValue);
   };
 
   return (
-    <form role="form" name="change-color">
-      <label htmlFor="hex-color" hidden>
-        Hex Color
-      </label>
-      <input
-        type="text"
-        id="hex-color"
-        name="hex-color"
-        aria-label="hex color"
-        value={hexValue}
-        onChange={handleInputChange}
-        ref={inputRef}
-      />
-    </form>
+    <div>
+      <form aria-label="change-color">
+        <label htmlFor="hex" hidden>
+          Hex Color
+        </label>
+        <input
+          id="hex"
+          name="hex color"
+          value={hexValue}
+          onChange={handleInputChange}
+          ref={inputRef}
+        />
+      </form>
+    </div>
   );
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header";
+import ChangeColor from "./components/changeColor";
 
 export default function Home() {
   return (
     <main>
       < Header/>
+      <ChangeColor/>
     </main>
   )
 }
